2674037cd20897f060e4d6a601abea47d5d88ac4
SmailyBarath <115382856+SmailyBarath@users.noreply.github.com>
2022-12-03 19:05:54 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -22,7 +22,7 @@ bot = Client('bot',
              plugins=dict(root=f"{__name__}/plugins"))
 
 
-bot = Client(
+darbot = Client(
     api_id=os.environ.get('API_ID'),
     api_hash=os.environ['API_HASH'],
 )
