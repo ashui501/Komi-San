e022d17fdec7144a256f4bcc9e3e2d6b8733760d
SmailyBarath <115382856+SmailyBarath@users.noreply.github.com>
2022-12-03 18:58:07 +0530

    Done

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -1,5 +1,4 @@
 from pyrogram import filters, Client
-from redis import Redis
 import os
 
 help_message = []
@@ -22,10 +21,8 @@ bot = Client('bot',
              bot_token=os.environ['BOT_TOKEN'],
              plugins=dict(root=f"{__name__}/plugins"))
 
-PYRO_SESSION = os.environ['PYRO_SESSION']
 
-musicbot = Client(
-    PYRO_SESSION,
+bot = Client(
     api_id=os.environ.get('API_ID'),
     api_hash=os.environ['API_HASH'],
 )
